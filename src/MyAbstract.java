99449441cb38648c595fc6e814eb12598036ff19
Noah Buchanan <96553082+NoahB7@users.noreply.github.com>
2022-02-17 20:40:45 -0600

    Add files via upload

---
+++ src/MyAbstract.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+
+/* ********************************************************
+* Student Name: Noah Buchanan
+* Class: CS 1024 Foundations of Programming II
+* Assignment: Final Exam
+* Date: May 4, 2020
+* ********************************************************/
+
+public abstract class MyAbstract {
+
+
+    public abstract void displayAllFirstNames() throws IOException;
+
+
+    public abstract int displayAllStudentAgeAvg() throws IOException;
+
+
+    public abstract boolean findAStudentByName(String nameToSearch) throws IOException;
+
+
+    public abstract String[] displayAllStudentInformationOlderThan18() throws IOException;
+
+
+}
+
